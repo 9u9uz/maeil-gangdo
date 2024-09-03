3f7c51f2ace7c6433f22cebd4b83723a44b94970
Dahee Goo <dadaahee@gmail.com>
2024-09-03 18:29:02 +0900

    chore: init backend project, add gitignore

---
+++ backend/maegang/src/main/java/com/guguz/maegang/MaegangApplication.java
@@ -0,0 +1,13 @@
+package com.guguz.maegang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaegangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaegangApplication.class, args);
+	}
+
+}
---
+++ backend/maegang/src/test/java/com/guguz/maegang/MaegangApplicationTests.java
@@ -0,0 +1,9 @@
+package com.guguz.maegang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaegangApplicationTests {
+
+}

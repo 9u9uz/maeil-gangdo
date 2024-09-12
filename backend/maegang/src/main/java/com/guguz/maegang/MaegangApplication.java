dda213784d5ed14cc733cd46f7d442aaf10feab7
Dahee Goo <dadaahee@gmail.com>
2024-09-12 18:17:18 +0900

    chore: 코드 스타일링 설정 추가 (네이버, checkstyle, spotless)

--- backend/maegang/src/main/java/com/guguz/maegang/MaegangApplication.java
+++ backend/maegang/src/main/java/com/guguz/maegang/MaegangApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MaegangApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MaegangApplication.class, args);
 	}
-
 }
--- backend/maegang/src/test/java/com/guguz/maegang/MaegangApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.guguz.maegang;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MaegangApplicationTests {
-
-}

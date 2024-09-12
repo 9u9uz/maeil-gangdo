6bc9db1ac696b2c326a96781b4479ba01d2dfc96
Dahee Goo <dadaahee@gmail.com>
2024-09-13 03:19:32 +0900

    fix: 들여쓰기 Checkstyle 규칙 통일

--- backend/maegang/src/main/java/com/guguz/maegang/MaegangApplication.java
+++ backend/maegang/src/main/java/com/guguz/maegang/MaegangApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MaegangApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MaegangApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MaegangApplication.class, args);
+    }
 }
